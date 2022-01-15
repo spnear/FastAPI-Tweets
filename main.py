040edd94053566b415864dda83f6cb13b84c1080
Sebastian <juansebastian5397@gmail.com>
2022-01-15 13:35:26 -0500

    Finalizado tutorial API imitando tweets

--- main.py
+++ main.py
@@ -180,7 +180,28 @@ def update():
     tags=["Tweets"]
 )
 def home():
-    return {"Twitter API": "Working!"}
+    """
+    This path operation shows all tweets in the app
+
+    Parameters: 
+        -
+
+    Returns a json list with all tweets in the app, with the following keys: 
+
+        - tweet_id: UUID
+
+        - content: str 
+
+        - created_at: datetime
+
+        - update_at: Optional[datetime]
+
+        - by: User
+
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f: 
+        results = json.loads(f.read())
+        return results
 
 
 @app.post(
